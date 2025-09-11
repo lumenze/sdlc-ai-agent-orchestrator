e93975a92dccd2548b634c7b163dc7a34b5ce412
Sharon Bose <bose@lumenze.com>
2025-09-11 13:04:15 -0700

    Fix test client import and remove broken helper import

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from src.api.routes.health import router
+from src.api.main import app
 
 app = FastAPI()
-app.include_router(router)
+
 
 client = TestClient(app)
 
