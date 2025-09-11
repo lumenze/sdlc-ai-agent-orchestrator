e2249e575746195d7bae5c76514d30f306e4822c
Sharon Bose <bose@lumenze.com>
2025-09-11 13:13:15 -0700

    modification to test to delete unnecessary object initiation

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,8 +1,6 @@
 from fastapi.testclient import TestClient
 from src.api.main import app  # This should already include the router
 
-client = TestClient(app)
-
 def test_health_check():
     response = client.get("/health")
     assert response.status_code == 200
